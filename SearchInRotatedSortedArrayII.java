06e811732ae5ae0c9a5beb60f41f3da5d31e3194
Anushka Agrawal <anushkaagrawal052@gmail.com>
2026-03-26 22:23:29 +0530

    Add files via upload

---
+++ SearchInRotatedSortedArrayII.java
@@ -0,0 +1,46 @@
+public class SearchInRotatedSortedArrayII {
+     public static boolean search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            // target found
+            if (nums[mid] == target) {
+                return true;
+            }
+
+            // handle duplicates
+            if (nums[low] == nums[mid] && nums[mid] == nums[high]) {
+                low++;
+                high--;
+            }
+            // left half is sorted
+            else if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && target < nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
+            // right half is sorted
+            else {
+                if (nums[mid] < target && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 5, 6, 0, 0, 1, 2};
+        int target = 0;
+
+        System.out.println(search(nums, target));  // true
+    }
+}
